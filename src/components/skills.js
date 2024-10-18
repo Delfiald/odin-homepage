25d31fcb5cd755a2749273776e018e5af805bdbf
M.Aldi Gunawan <aldigunawan1808@gmail.com>
2024-10-18 15:56:00 +0700

    Create Skills Section
    - Add header and container for skills section
    - Inside container, add 5 wrappers
    - Each wrapper contains 12 small boxes to display individual skills

--- src/components/skills.js
+++ src/components/skills.js
@@ -1,6 +1,40 @@
+const createSkills = () => {
+  const skill = document.createElement('div')
+  skill.className = 'skill'
+
+  return skill
+}
+
+const createSkillsWrapper = () => {
+  const skillsWrapper = document.createElement('div')
+  skillsWrapper.className = 'skills-wrapper'
+
+  for(let i = 0; i < 12; i+=1) {
+    skillsWrapper.appendChild(createSkills())
+  }
+
+  return skillsWrapper
+}
+
 const createSkillSection = () => {
   const skillSection = document.createElement('section')
+  skillSection.className = 'skills'
+  const skillHeader = document.createElement('h1')
+  skillHeader.textContent = 'SKILLS'
+  skillHeader.className = 'skills-header header'
 
+  skillSection.appendChild(skillHeader);
+
+  const skillContainer = document.createElement('div')
+  skillContainer.className = 'skill-container'
+  
+  skillContainer.appendChild(createSkillsWrapper())
+  skillContainer.appendChild(createSkillsWrapper())
+  skillContainer.appendChild(createSkillsWrapper())
+  skillContainer.appendChild(createSkillsWrapper())
+  skillContainer.appendChild(createSkillsWrapper())
+
+  skillSection.appendChild(skillContainer)
   return skillSection
 }
 
