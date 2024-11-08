ef53e77148f4f62133b6ac1eaba35be12712a04b
M.Aldi Gunawan <aldigunawan1808@gmail.com>
2024-11-08 20:42:59 +0700

    Create Skills Descriptions
    - add skills descriptions that will show when hovering

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,18 +1,29 @@
 // Screenshot
-import projectsSS from '../assets/projects/image.png'
+import projectsSS from '../assets/projects/image.png';
 
 // Techs
-import techsData from './techsData'
+import techsData from './techsData';
 
 const getIcon = (techName) => {
   const techs = techsData();
-  return techs[techName]?.icon;
-}
+  return {
+    icon: techs[techName]?.icon,
+    description: techs[techName]?.description,
+  };
+};
 
 const projectsData = (() => {
-  const projects = []
+  const projects = [];
 
-  const addProjects = (screenshot, title, summary, features, techs, demo, repos) => {
+  const addProjects = (
+    screenshot,
+    title,
+    summary,
+    features,
+    techs,
+    demo,
+    repos
+  ) => {
     const projectsObj = {
       id: projects.length + 1,
       screenshot,
@@ -21,113 +32,140 @@ const projectsData = (() => {
       features,
       techs,
       demo,
-      repos
-    }
+      repos,
+    };
 
-    projects.push(projectsObj)
-  }
+    projects.push(projectsObj);
+  };
 
   const getProjects = () => projects;
 
   return {
     addProjects,
-    getProjects
-  }
-})()
+    getProjects,
+  };
+})();
 
 const projectsList = () => {
   // Projects 1
   projectsData.addProjects(
-    projectsSS, 
-    'test', 
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur minus reiciendis aspernatur sint tenetur et culpa hic. Harum, quia delectus.', 
-    ['1. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, quas!', '2. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, quas!'], 
-    [getIcon('js'), getIcon('html'), getIcon('css'), getIcon('webpack')], 
-    '#', 
+    projectsSS,
+    'test',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur minus reiciendis aspernatur sint tenetur et culpa hic. Harum, quia delectus.',
+    [
+      '1. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, quas!',
+      '2. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, quas!',
+    ],
+    [getIcon('js'), getIcon('html'), getIcon('css'), getIcon('webpack')],
+    '#',
     '#'
-  )
+  );
   // Projects 2
   projectsData.addProjects(
-    projectsSS, 
-    'test', 
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur minus reiciendis aspernatur sint tenetur et culpa hic. Harum, quia delectus.', 
-    ['1. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, quas!', '2. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, quas!'], 
-    [getIcon('js'), getIcon('html'), getIcon('css'), getIcon('webpack')], 
-    '#', 
+    projectsSS,
+    'test',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur minus reiciendis aspernatur sint tenetur et culpa hic. Harum, quia delectus.',
+    [
+      '1. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, quas!',
+      '2. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, quas!',
+    ],
+    [getIcon('js'), getIcon('html'), getIcon('css'), getIcon('webpack')],
+    '#',
     '#'
-  )
+  );
   // Projects 3
   projectsData.addProjects(
-    projectsSS, 
-    'test', 
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur minus reiciendis aspernatur sint tenetur et culpa hic. Harum, quia delectus.', 
-    ['1. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, quas!', '2. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, quas!'], 
-    [getIcon('js'), getIcon('html'), getIcon('css'), getIcon('webpack')], 
-    '#', 
+    projectsSS,
+    'test',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur minus reiciendis aspernatur sint tenetur et culpa hic. Harum, quia delectus.',
+    [
+      '1. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, quas!',
+      '2. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, quas!',
+    ],
+    [getIcon('js'), getIcon('html'), getIcon('css'), getIcon('webpack')],
+    '#',
     '#'
-  )
+  );
   // Projects 4
   projectsData.addProjects(
-    projectsSS, 
-    'test', 
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur minus reiciendis aspernatur sint tenetur et culpa hic. Harum, quia delectus.', 
-    ['1. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, quas!', '2. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, quas!'], 
-    [getIcon('js'), getIcon('html'), getIcon('css'), getIcon('webpack')], 
-    '#', 
+    projectsSS,
+    'test',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur minus reiciendis aspernatur sint tenetur et culpa hic. Harum, quia delectus.',
+    [
+      '1. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, quas!',
+      '2. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, quas!',
+    ],
+    [getIcon('js'), getIcon('html'), getIcon('css'), getIcon('webpack')],
+    '#',
     '#'
-  )
+  );
   // Projects 5
   projectsData.addProjects(
-    projectsSS, 
-    'test', 
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur minus reiciendis aspernatur sint tenetur et culpa hic. Harum, quia delectus.', 
-    ['1. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, quas!', '2. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, quas!'], 
-    [getIcon('js'), getIcon('html'), getIcon('css'), getIcon('webpack')], 
-    '#', 
+    projectsSS,
+    'test',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur minus reiciendis aspernatur sint tenetur et culpa hic. Harum, quia delectus.',
+    [
+      '1. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, quas!',
+      '2. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, quas!',
+    ],
+    [getIcon('js'), getIcon('html'), getIcon('css'), getIcon('webpack')],
+    '#',
     '#'
-  )
+  );
   // Projects 6
   projectsData.addProjects(
-    projectsSS, 
-    'test', 
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur minus reiciendis aspernatur sint tenetur et culpa hic. Harum, quia delectus.', 
-    ['1. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, quas!', '2. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, quas!'], 
-    [getIcon('js'), getIcon('html'), getIcon('css'), getIcon('webpack')], 
-    '#', 
+    projectsSS,
+    'test',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur minus reiciendis aspernatur sint tenetur et culpa hic. Harum, quia delectus.',
+    [
+      '1. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, quas!',
+      '2. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, quas!',
+    ],
+    [getIcon('js'), getIcon('html'), getIcon('css'), getIcon('webpack')],
+    '#',
     '#'
-  )
+  );
   // Projects 7
   projectsData.addProjects(
-    projectsSS, 
-    'test', 
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur minus reiciendis aspernatur sint tenetur et culpa hic. Harum, quia delectus.', 
-    ['1. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, quas!', '2. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, quas!'], 
-    [getIcon('js'), getIcon('html'), getIcon('css'), getIcon('webpack')], 
-    '#', 
+    projectsSS,
+    'test',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur minus reiciendis aspernatur sint tenetur et culpa hic. Harum, quia delectus.',
+    [
+      '1. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, quas!',
+      '2. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, quas!',
+    ],
+    [getIcon('js'), getIcon('html'), getIcon('css'), getIcon('webpack')],
+    '#',
     '#'
-  )
+  );
   // Projects 8
   projectsData.addProjects(
-    projectsSS, 
-    'test', 
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur minus reiciendis aspernatur sint tenetur et culpa hic. Harum, quia delectus.', 
-    ['1. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, quas!', '2. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, quas!'], 
-    [getIcon('js'), getIcon('html'), getIcon('css'), getIcon('webpack')], 
-    '#', 
+    projectsSS,
+    'test',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur minus reiciendis aspernatur sint tenetur et culpa hic. Harum, quia delectus.',
+    [
+      '1. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, quas!',
+      '2. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, quas!',
+    ],
+    [getIcon('js'), getIcon('html'), getIcon('css'), getIcon('webpack')],
+    '#',
     '#'
-  )
+  );
   // Projects 9
   projectsData.addProjects(
-    projectsSS, 
-    'test', 
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur minus reiciendis aspernatur sint tenetur et culpa hic. Harum, quia delectus.', 
-    ['1. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, quas!', '2. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, quas!'], 
-    [getIcon('js'), getIcon('html'), getIcon('css'), getIcon('webpack')], 
-    '#', 
+    projectsSS,
+    'test',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur minus reiciendis aspernatur sint tenetur et culpa hic. Harum, quia delectus.',
+    [
+      '1. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, quas!',
+      '2. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, quas!',
+    ],
+    [getIcon('js'), getIcon('html'), getIcon('css'), getIcon('webpack')],
+    '#',
     '#'
-  )
-}
+  );
+};
 
-projectsList()
+projectsList();
 
-export default projectsData
+export default projectsData;
